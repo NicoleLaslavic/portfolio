acb9b3503ea70ccd849b9297f413340a45b274a5
Nicole Laslavic <lasl7800@mylaurier.ca>
2022-01-30 13:58:16 -0500

    test

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 // https://NicoleLaslavic.github.io/portfolio
+// npm run deploy to update
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nicole Laslavic</h1>
     </div>
   );
 }
